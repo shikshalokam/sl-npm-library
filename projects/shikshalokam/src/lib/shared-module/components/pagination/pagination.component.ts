b0dbe9fd6619e3b31e4f8621cc86ecf2b022508d
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-03-27 19:00:01 +0530

    pagination error fixed

--- projects/shikshalokam/src/lib/shared-module/components/pagination/pagination.component.ts
+++ projects/shikshalokam/src/lib/shared-module/components/pagination/pagination.component.ts
@@ -11,6 +11,8 @@ export class PaginationComponent implements OnInit {
   @Input() length;
   @Input() paginationLabel;
   @Input() itemsPerPage;
+  @Input() pageLimit;
+  @Input() pageIndex;
   pagination;
   last;
   selected;
@@ -18,7 +20,7 @@ export class PaginationComponent implements OnInit {
   paginationFlag = false;
   data;
   label;
-  startList=1;
+  startList;
   endList;
   @Output() paginationResponse = new EventEmitter();
   constructor(private _fb: FormBuilder) {
@@ -26,16 +28,27 @@ export class PaginationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.endList = this.itemsPerPage[0] ;
+    if(this.pageIndex){
+      this.startList = ( this.pageIndex ) * this.pageLimit + 1;
+      if(this.length - this.startList > this.pageLimit)
+      this.endList = this.startList + this.pageLimit -1;
+      else {
+      this.endList = this.startList + (this.length - this.startList );
+      }
+    }
+   else{
+     this.startList = 1;
+     this.endList = this.startList + this.pageLimit -1;
+   }
     this.label = "Showing "+this.startList + ' - '+this.endList +" out of "+this.length +" "+this.paginationLabel;
-    this.last = this.length / this.itemsPerPage[0]  -1 ;
-    if( (this.length % this.itemsPerPage [0])  !== 0   ){
+    this.last = this.length / this.pageLimit  -1 ;
+    if( (this.length % this.pageLimit)  !== 0   ){
      this.last =  Math.floor(this.last) +1 ;
     }
-    this.selected = this.itemsPerPage[0];
+    this.selected = this.pageLimit ? this.pageLimit : this.itemsPerPage[0];
     this.pagination = {
       previousPageIndex: 0,
-      pageIndex: 0,
+      pageIndex: this.pageIndex ,
       pageLimit: this.selected,
       length: this.length
     }
@@ -44,7 +57,7 @@ export class PaginationComponent implements OnInit {
       editable: true,
       field: "pageLimit",
       input: "dropdown",
-      label: "",
+      label: this.pageLimit,
       validation: { required: true },
       value: this.itemsPerPage[0],
       visible: true,
@@ -68,28 +81,32 @@ export class PaginationComponent implements OnInit {
       case 'back':{
         this.pagination.previousPageIndex = this.pagination.pageIndex;
         this.pagination.pageIndex -= 1;
+        this.pageIndex = this.pagination.pageIndex ;
         break;
       }
       case 'next':{
         this.pagination.previousPageIndex = this.pagination.pageIndex;
       this.pagination.pageIndex += 1;
+      this.pageIndex = this.pagination.pageIndex ;
       break;
       }
       case 'first':{
         this.pagination.previousPageIndex = 0;
         this.pagination.pageIndex = 0;
+        this.pageIndex = this.pagination.pageIndex ;
         break;
       }
       case 'last':{
         this.pagination.previousPageIndex = this.last;
       this.pagination.pageIndex = this.pagination.previousPageIndex;
+      this.pageIndex = this.pagination.pageIndex ;
       break;
       }
     }
    
-    this.startList = ( this.pagination.pageIndex )* this.pagination.pageLimit + 1;
-    if(this.length - this.startList > this.pagination.pageLimit)
-         this.endList = this.startList + this.pagination.pageLimit -1;
+    this.startList = ( this.pageIndex ) * this.pageLimit + 1;
+    if(this.length - this.startList > this.pageLimit)
+         this.endList = this.startList + this.pageLimit -1;
     else {
       this.endList = this.startList + (this.length - this.startList );
 
