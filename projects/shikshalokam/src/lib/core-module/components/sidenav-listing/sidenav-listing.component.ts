2cf29d06d17130bb584172a718e55e2b478589c6
srikanthgudipudi <gadipudisrikanth654@gmail.com>
2020-10-16 21:10:07 +0530

    side menu collapse

--- projects/shikshalokam/src/lib/core-module/components/sidenav-listing/sidenav-listing.component.ts
+++ projects/shikshalokam/src/lib/core-module/components/sidenav-listing/sidenav-listing.component.ts
@@ -7,11 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidenav-listing.component.scss']
 })
 export class SideNavListingComponent implements OnInit {
+  expandedIndex: any;
   @Input() libraryMenuData;
   @Output() sideNavListClick = new EventEmitter();
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.expandedIndex = -1
+  }
+
+  // logic to show and hide the submenu
+  collapse(index: number) {
+    this.expandedIndex = index === this.expandedIndex ? -1 : index;
   }
 
 
